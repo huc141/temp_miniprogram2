688181fefbfe4684e01702d2a14528dae7e30777
huc141 <1349983371@qq.com>
2025-04-09 23:59:30 +0800

    本地存储加注释

--- pages/local_storage/local_storage.js
+++ pages/local_storage/local_storage.js
@@ -13,14 +13,17 @@ Page({
                 wx.navigateBack();
         },
 
-        inputChange(e) {
-                this.setData({
+        inputChange(e) { 
+                this.setData({// 双向绑定，当inputValue的值发生变化时，会触发inputChange事件,this.setData()是小程序更新页面数据的方法
+                        // 获取输入框的值，并赋值给inputValue
                         inputValue: e.detail.value
                 });
         },
 
         storageData() {
+                // 获取输入框的值，并赋值给data
                 const data = this.data.inputValue;
+                // 如果输入框的值为空，则弹出提示
                 if (!data || data.trim() === '') {
                         wx.showToast({
                                 title: '请输入内容',
@@ -29,7 +32,7 @@ Page({
                         });
                         return;
                 }
-                
+                // 将输入框的值存储到本地存储中
                 wx.setStorageSync('data', data);
                 wx.showToast({
                         title: '数据存储成功',
